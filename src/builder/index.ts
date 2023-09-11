efcc41f11b8c6068d2c9fd6edba964eec3a6cfc5
JMTiburcio <jmrtme@gmail.com>
2023-09-10 23:40:02 -0300

    creating builder and director

---
+++ src/builder/index.ts
@@ -0,0 +1,54 @@
+class Product {
+  propertyA?: string;
+  propertyB?: string;
+  propertyC?: string;
+}
+
+export class Builder {
+  private product: Product;
+
+  constructor() {
+    this.product = new Product();
+  }
+
+  public setPropertyA(property: string): this {
+    this.product.propertyA = property;
+    return this;
+  }
+
+  public setPropertyB(property: string): this {
+    this.product.propertyB = property;
+    return this;
+  }
+
+  public setPropertyC(property: string): this {
+    this.product.propertyC = property;
+    return this;
+  }
+
+  public build(): Product {
+    const product = this.product;
+    this.reset();
+    return product;
+  }
+
+  private reset(): void {
+    this.product = new Product();
+  }
+}
+
+export class Director {
+  constructor(private readonly builder: Builder) {}
+
+  public buildProductWithAllProperties(): Product {
+    return this.builder.setPropertyA("A").setPropertyB("B").setPropertyC("C").build();
+  }
+
+  public buildProductWithProperyA(): Product {
+    return this.builder.setPropertyA("A").build();
+  }
+
+  public buildProductWithProperyC(): Product {
+    return this.builder.setPropertyC("C").build();
+  }
+}
---
+++ tests/builder/index.test.ts
@@ -0,0 +1,41 @@
+import { Builder, Director } from "../../src/builder";
+
+describe("Builder", () => {
+  it("should build a product with all properties", () => {
+    const builder = new Builder();
+
+    const product = builder.setPropertyA("A").setPropertyB("B").setPropertyC("C").build();
+    expect(product).toEqual({ propertyA: "A", propertyB: "B", propertyC: "C" });
+  });
+  it("should build multiple products", () => {
+    const builder = new Builder();
+
+    const product1 = builder.setPropertyA("A").setPropertyB("B").setPropertyC("C").build();
+    const product2 = builder.setPropertyA("A").setPropertyB("B").build();
+    expect(product1).toEqual({ propertyA: "A", propertyB: "B", propertyC: "C" });
+    expect(product2).toEqual({ propertyA: "A", propertyB: "B" });
+  });
+});
+
+describe("Director", () => {
+  it("should build a product with all properties", () => {
+    const builder = new Builder();
+    const director = new Director(builder);
+
+    const product = director.buildProductWithAllProperties();
+    expect(product).toEqual({ propertyA: "A", propertyB: "B", propertyC: "C" });
+  });
+
+  it("should build multiple products", () => {
+    const builder = new Builder();
+    const director = new Director(builder);
+
+    const product1 = director.buildProductWithAllProperties();
+    const product2 = director.buildProductWithProperyA();
+    const product3 = director.buildProductWithProperyC();
+
+    expect(product1).toEqual({ propertyA: "A", propertyB: "B", propertyC: "C" });
+    expect(product2).toEqual({ propertyA: "A" });
+    expect(product3).toEqual({ propertyC: "C" });
+  });
+});
