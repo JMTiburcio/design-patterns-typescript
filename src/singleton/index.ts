1f9173386d858fd3d0f796d605ab3c3007f685b5
JMTiburcio <jmrtme@gmail.com>
2023-09-10 21:22:53 -0300

    creating singleton

---
+++ src/singleton/index.ts
@@ -0,0 +1,12 @@
+export class Singleton {
+  private static instance: Singleton | null = null;
+
+  private constructor() {}
+
+  public static getInstance(): Singleton {
+    if (Singleton.instance === null) {
+      Singleton.instance = new Singleton();
+    }
+    return Singleton.instance;
+  }
+}
---
+++ tests/singleton/index.test.ts
@@ -0,0 +1,10 @@
+import { Singleton } from "../../src/singleton";
+
+describe("Singleton", () => {
+  it("should have only one instance", () => {
+    const singleton1 = Singleton.getInstance();
+    const singleton2 = Singleton.getInstance();
+
+    expect(singleton1 === singleton2).toBeTruthy();
+  });
+});
